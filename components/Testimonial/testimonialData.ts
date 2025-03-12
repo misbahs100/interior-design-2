dca34191c2596e6bf3cb56792e7bf14cb30c5454
misbahs100 <misbahuddins100@gmail.com>
2025-03-12 11:13:29 +0600

    modified testimonial importing

--- components/Testimonial/testimonialData.ts
+++ components/Testimonial/testimonialData.ts
@@ -1,5 +1,5 @@
-import image1 from "@/public/images/user/user-01.png";
-import image2 from "@/public/images/user/user-02.png";
+// import image1 from "@/public/images/user/user-01.png";
+// import image2 from "@/public/images/user/user-02.png";
 import { Testimonial } from "@/types/testimonial";
 
 export const testimonialData: Testimonial[] = [
@@ -7,7 +7,7 @@ export const testimonialData: Testimonial[] = [
     id: 1,
     name: "MD Mohibbullah",
     designation: "Founter @ Al-Maghrib International School",
-    image: image1,
+    image: "/images/user/user-01.png",
     content:
       "Their design transformed our classrooms into vibrant, engaging spaces. Exceptional work that truly enhances our learning environment!",
   },
@@ -15,7 +15,7 @@ export const testimonialData: Testimonial[] = [
     id: 2,
     name: "Rezaul Karim Chowdhury",
     designation: "Ex Mayor @ Chattogram",
-    image: image2,
+    image: "/images/user/user-02.png",
     content:
       "আমি তাদের সাথে কাজ করে খুব সন্তুষ্ট।",
   },
@@ -23,7 +23,7 @@ export const testimonialData: Testimonial[] = [
     id: 3,
     name: "মুহাম্মদ নজরুল ইসলাম",
     designation: "",
-    image: image1,
+    image: "/images/user/user-01.png",
     content:
       "তারা এত সুন্দরভাবে কাজ করেছে। আমি সন্তুষ্ট.",
   },
@@ -31,7 +31,7 @@ export const testimonialData: Testimonial[] = [
     id: 4,
     name: "Akbar Shah",
     designation: "Chairman @ LT International",
-    image: image2,
+    image: "/images/user/user-02.png",
     content:
       "ইনটেরিয়র ডিজাইনার চট্টগ্রাম জিপসাম একটি সুন্দর পরিবেশ তৈরি করেছে। আমাদের গ্রাহকরা তাদের দ্বারা করা নতুন ডিজাইন পছন্দ করেছেন",
   },
